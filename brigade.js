01cf45164018fc5ddfcb76a97bf50fd998d01009
Eduardo <eduardo@translucentcomputing.com>
2018-03-09 09:32:42 -0300

    Run multiple jobs

--- brigade.js
+++ brigade.js
@@ -1,7 +1,28 @@
-const { events } = require("brigadier");
+const { events, Job, Group } = require("brigadier")
 
-events.on("push", function(e, project) {
-  console.log("received push for commit " + e.commit)
+events.on("push", () => {
+  var hello = new Job("hello", "alpine:3.4", ["echo hello"])
+  var goodbye = new Job("goodbye", "alpine:3.4", ["echo goodbye"])
+  var helloAgain = new Job("hello-again", "alpine:3.4", ["echo hello again"])
 
-  console.log(e);
+  console.log("running")
+
+  Group.runAll([hello, goodbye])
+    .then( ()=> {
+      helloAgain.run()
+    })
+})
+
+
+events.on("exec", () => {
+  var hello = new Job("hello", "alpine:3.4", ["echo hello"])
+  var goodbye = new Job("goodbye", "alpine:3.4", ["echo goodbye"])
+  var helloAgain = new Job("hello-again", "alpine:3.4", ["echo hello again"])
+
+  console.log("running")
+  
+  Group.runAll([hello, goodbye])
+    .then( ()=> {
+      helloAgain.run()
+    })
 })
